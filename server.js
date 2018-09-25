7d209a24ff11ecdd7cd457c8da13e178cd5692b8
andventuree <awwong92@gmail.com>
2018-09-25 13:21:18 -0400

    completed project

--- server.js
+++ server.js
@@ -3,13 +3,27 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "./uploads/" });
 const mongoose = require("mongoose");
 
 const app = express();
 
+// Connecting to mLab - MongoDB
 require("./secrets.js");
 mongoose.connect(process.env.MLAB_URI);
+
+// Multer configuration
+const multerConfig = {
+  storage: multer.diskStorage({
+    destination: (req, file, next) => {
+      next(null, "./uploads");
+    },
+    filename: (req, file, next) => {
+      const ext = file.mimetype.split("/")[1];
+      next(null, `${file.fieldname}-${Date.now()}-${ext}`);
+    }
+  })
+};
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -18,9 +32,42 @@ app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.get("/hello", function(req, res) {
-  res.json({ greetings: "Hello, API" });
+// MongoDB models and schemas
+
+const Schema = mongoose.Schema;
+const fileSchema = new Schema({
+  name: String, // name: "Andrew Wong Resume CV.pdf",
+  type: String, // type: "application/pdf",
+  size: Number // size: 242873
 });
+
+const File = mongoose.model("file", fileSchema);
+
+// Helper Function
+
+const parseFileData = data => {
+  return {
+    name: data.originalname,
+    type: data.mimetype,
+    size: data.size
+  };
+};
+
+// API endpoints
+
+// POST /api/fileanalyse
+app.post(
+  "/api/fileanalyse",
+  multer(multerConfig).single("upfile"),
+  (req, res, next) => {
+    File.create(parseFileData(req.file), (err, data) => {
+      if (err) next(err);
+      let { name, type, size } = data;
+      const metadata = { name, type, size };
+      res.status(200).json(metadata);
+    });
+  }
+);
 
 const listener = app.listen(process.env.PORT || 3000, function() {
   console.log("Node.js listening ...", listener.address().port);
