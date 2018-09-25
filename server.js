fb50040d387aecf7b0720783c3f3cb98da61c294
andventuree <awwong92@gmail.com>
2018-09-25 12:27:29 -0400

    installed dependencies and connected DB

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const mongoose = require("mongoose");
 
-// require and use "multer"...
+const app = express();
 
-var app = express();
+require("./secrets.js");
+mongoose.connect(process.env.MLAB_URI);
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get("/hello", function(req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+const listener = app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...", listener.address().port);
 });
