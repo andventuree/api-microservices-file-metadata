7bd5fa00991ae755891fc698ca74d5eeac8f280a
andventuree <awwong92@gmail.com>
2018-09-25 15:39:31 -0400

    removed comments

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-// const upload = multer({ dest: "./uploads/" });
 const mongoose = require("mongoose");
 
 const app = express();
